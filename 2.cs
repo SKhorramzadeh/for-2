8e68fbd219f736cf4d6103f52eaed8f9b1fb6c1a
SKhorramzadeh <s.khorramzadeh@gmail.com>
2024-10-13 18:54:36 +0330

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,19 @@
+namespace Task_7_22
+{
+    internal class Program
+    {
+        //Task 1: Summation of two numbers.
+        static void Main(string[] args)
+        {
+		int j
+		string a3;
+		for (int i=0 ; i<10 ; i++)
+		{	
+		j=2*i
+		a3 = i3.ToString();
+		Console.Writeline($"The value is {a3} .");
+        }
+		
+
+    }
+}
